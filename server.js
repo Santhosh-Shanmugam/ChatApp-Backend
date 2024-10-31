f35388a1effed4593fa575f3ed8dafc834eee53c
Santhosh Shanmugam <santhoshs192005@gmail.com>
2024-10-31 09:14:13 +0530

    socket events enabled

--- server.js
+++ server.js
@@ -3,18 +3,33 @@ import http from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
 import router from "./router.js";  // Import the router module
+import cors  from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 2000;
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
 
+app.use(cors()); // This will enable CORS for any origin
 app.use(router);  // Use the router middleware
 
 io.on('connection', (socket)=>{ // default parameter
   console.log('a new client connected');
+
+
+  socket.on('join' ,({name,room},callback)=>{
+    
+    console.log(name,room);
+  })
+
 
   socket.on('disconnection' , ()=>{
     console.log('a client disconnected');
